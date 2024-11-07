5fc453260ebfed17314f291a630b731d3af9c3c5
Kevin Day <kday@tamu.edu>
2024-11-07 17:00:01 -0600

    WIP: 2024/11/07 - 2.

--- domain/src/main/java/org/folio/spring/domain/config/JacksonConfiguration.java
+++ domain/src/main/java/org/folio/spring/domain/config/JacksonConfiguration.java
@@ -2,6 +2,7 @@ package org.folio.spring.domain.config;
 
 import com.fasterxml.jackson.core.StreamReadFeature;
 import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.MapperFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.json.JsonMapper;
 import org.springframework.context.annotation.Bean;
@@ -14,6 +15,7 @@ public class JacksonConfiguration {
   public ObjectMapper objectMapper() {
     return JsonMapper.builder()
       .enable(StreamReadFeature.INCLUDE_SOURCE_IN_LOCATION)
+      .enable(MapperFeature.REQUIRE_TYPE_ID_FOR_SUBTYPES)
       .disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES)
       .build();
   }
